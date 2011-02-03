3aa851e5cdc10e41c232f9842ddc57170a1a21e8
Matthew Schiros <schiros@invisihosting.com>
2011-02-03 13:38:12 -0600

    README updated, some updates to code generator

--- skeet/lib/skeet/components/generator/starter_model_template.inc.php
+++
@@ -1,9 +0,0 @@
-<?
-
-?><?= '<?php' ?>
-	namespace Skeet\Model;
-	
-	class <?= $classSetupArray["class_name"] ?> extends \Skeet\Generated\Model\<?= $classSetupArray["class_name"] ?>Generated {
-	
-	}
-?>
